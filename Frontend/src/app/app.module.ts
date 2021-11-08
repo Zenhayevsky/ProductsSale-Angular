94de6c1c3aa7b20e1740e5cbf2a454dff3761a84
Zenhayevsky Carolina Solorzano Acevedo <carolina.acevedo@rgm.com.br>
2021-11-08 14:56:58 -0300

    footer e header prontos

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -7,17 +7,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 
 import { MatToolbar, MatToolbarModule } from '@angular/material/toolbar';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
